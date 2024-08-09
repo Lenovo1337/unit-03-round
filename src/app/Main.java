360f3409da01a00f5c744541ba2787d8e761336c
Lenovo <sheets.vladimir1@gmail.com>
2024-08-09 16:22:10 +0200

    https://github.com/Lenovo1337/unit-03-round.git

---
+++ src/app/Main.java
@@ -0,0 +1,29 @@
+package app;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+        int min = 1;
+        int max = 6;
+        int sum = 0;
+
+
+        for (int a = min; a <= max; a++) {
+            sum += a;
+
+            System.out.print("Num is " + a); System.out.println ("Sum is " + sum);
+
+
+
+
+
+            }
+        }
+    }
